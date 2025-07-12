148a785c200f4d6df9f33e631b2183505b7faf7f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-12 06:15:54 +0300

    fix: correct variable name for trigger height in scroll animation function

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const boxes = document.querySelectorAll('.box');
 
 const checkboxes = () => {
-	const triggerHight = (window.innerHeight / 5) * 4;
+	const triggerBottom = (window.innerHeight / 5) * 4;
+
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+
+		if (boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
 };
 
 window.addEventListener('scroll', checkboxes);
+checkboxes();
