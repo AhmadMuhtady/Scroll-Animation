e0cf8d1511992d1b5f1a300211fbb6cd58d091fe
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-07-12 06:03:23 +0300

    add checkboxes function to handle scroll event for animation triggers

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const boxes = document.querySelectorAll('.box');
 
+const checkboxes = () => {
+	const triggerHight = (window.innerHeight / 5) * 4;
+};
+
 window.addEventListener('scroll', checkboxes);
